72ced887c9532691b9d28d554f04512b712640d4
Mohammad Salim <mohammadsalim017427@gmail.com>
2024-06-30 07:10:07 -0700

    Basic setup

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,18 @@
+import { Outlet, useLocation } from "react-router-dom";
+import Navbar from "../shared/Navbar";
+import Footer from "../shared/Footer";
+
+
+const Main = () => {
+    const location =useLocation();
+    const noHeaderFooter = location.pathname.includes('login')  || location.pathname.includes('signup');
+    return (
+        <div>
+            {noHeaderFooter || <Navbar></Navbar>}
+            <Outlet></Outlet>
+            {noHeaderFooter || <Footer></Footer>}
+        </div>
+    );
+};
+
+export default Main;
