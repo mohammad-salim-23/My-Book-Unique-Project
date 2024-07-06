ec0fd880603deaa7a1745b1452a10b8ea3952d29
Mohammad Salim <mohammadsalim017427@gmail.com>
2024-07-06 23:01:20 +0600

    handleGoogleSignIn function create

--- src/components/SocialComponent.jsx
+++ src/components/SocialComponent.jsx
@@ -8,6 +8,20 @@ const SocialComponent = () => {
    const axiosPublic = useAxiosPublic();
    const navigate = useNavigate();
    const {googleSignIn} = useContext(AuthContext);
+   const handleGoogleSignIn = ()=>{
+     googleSignIn()
+     .then(result=>{
+        const userInfo={
+            email:result.user?.email,
+            name:result.user?.displayName
+        }
+        axiosPublic.post('/users',userInfo)
+        .then(res=>{
+            console.log(res.data);
+            navigate('/');
+        })
+     })
+   }
     return (
         <div className="px-32">
             <div>
