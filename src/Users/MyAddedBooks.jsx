186611eb2552b2ecf353a64c8653903ea0d6d82f
Mohammad Salim <mohammadsalim017427@gmail.com>
2024-07-14 23:37:39 +0600

    fetch added books data in the server based on the email

--- src/Users/MyAddedBooks.jsx
+++ src/Users/MyAddedBooks.jsx
@@ -1,6 +1,30 @@
+import { useContext, useState } from "react";
+import { AuthContext } from "../provider/AuthProvider";
+import useAxiosSecure from "../hooks/useAxiosSecure";
+import { useNavigate } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
 
 
 const MyAddedBooks = () => {
+    const {user} = useContext(AuthContext);
+    const axiosSecure = useAxiosSecure();
+    const navigate = useNavigate();
+    const [currentPage,setCurrentPage] = useState(1);
+    const itemsPerPage = 10;
+    const{
+        data:books=[],
+        isLoading,
+        isError,
+        error,
+
+    }= useQuery({
+           queryKey:['books',user?.email],
+           queryFn:async()=>{
+            const result = await axiosSecure.get(`/books/${user?.email}`);
+            return result.data;
+           },
+           enabled:!!user?.email,
+    });
     return (
         <div>
             
